ad1b38e76670940f9ffc2e05da4cadcbbc50dcf3
Saulo Silva Filho <saulo.vsfh@gmail.com>
2023-01-13 13:20:06 +0100

    1 process running from ide only

--- app_update.py
+++ app_update.py
@@ -56,28 +56,28 @@ repo.index.add(file_path)
 repo.git.commit( '-m', commit_message)
 
 
-# '''
-# Add to Github
-# '''
-# with open('D:\Repos\AccessToken.txt', 'r') as f:
-#     token = f.readline()
+'''
+Add to Github
+'''
+with open('D:\Repos\AccessToken.txt', 'r') as f:
+    token = f.readline()
 
-# g = Github(token)    
-# repo = g.get_user().get_repo('PirnaStudyCase')
-# master_ref = repo.get_git_ref('heads/master')
-# master_sha = master_ref.object.sha
-# base_tree = repo.get_git_tree(master_sha)
-# element_list = list()
+g = Github(token)    
+repo = g.get_user().get_repo('PirnaStudyCase')
+master_ref = repo.get_git_ref('heads/master')
+master_sha = master_ref.object.sha
+base_tree = repo.get_git_tree(master_sha)
+element_list = list()
 
-# with open(file_path) as input_file:
-#     data = input_file.read()
-#     if file_path.endswith('.png'): # images must be encoded
-#         data = base64.b64encode(data)
+with open(file_path) as input_file:
+    data = input_file.read()
+    if file_path.endswith('.png'): # images must be encoded
+        data = base64.b64encode(data)
        
-# element = InputGitTreeElement(file_path, '100644', 'blob', data)
-# element_list.append(element)
+element = InputGitTreeElement(file_path, '100644', 'blob', data)
+element_list.append(element)
 
-# tree = repo.create_git_tree(element_list, base_tree)
-# parent = repo.get_git_commit(master_sha)
-# commit = repo.create_git_commit(commit_message, tree, [parent])
-# master_ref.edit(commit.sha)
+tree = repo.create_git_tree(element_list, base_tree)
+parent = repo.get_git_commit(master_sha)
+commit = repo.create_git_commit(commit_message, tree, [parent])
+master_ref.edit(commit.sha)
