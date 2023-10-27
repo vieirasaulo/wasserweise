1f714f5994c2cde04c00aaefb8d15f6110f8cc78
Saulo Silva Vieira <s.vieira@tutamail.com>
2023-10-27 15:40:57 +0200

    Issue #3
    - [x]  Reset dependencies with Poetry and install it as a Python package - [x] add pre-commit and lintering

---
+++ src/wasserweise/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module to initiate this repo as a python package.
+All of the functionalities and packages that operate under the hoot will be imported through and accessed through this file
+"""
+
+import wasserweise.api
+import wasserweise.utils
+import wasserweise.queries
+import wasserweise.update
